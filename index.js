2d799b38d5982d49f25db88ae3df1c3bad14baab
siya <siyasri0604@gmail.com>
2026-04-24 18:46:57 +0530

    port fixint

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.post('/bfhl', (req, res) => {
   const data = req.body.data || [];
